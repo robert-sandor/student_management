968d0549fb5e8956f037f5ffee6acc0bdee79166
dutzi <alexandratrifan95@gmail.com>
2016-04-11 00:28:13 +0300

    display signed in user data

--- app/modules/professor_cod/controllers.py
+++ app/modules/professor_cod/controllers.py
@@ -1,5 +1,6 @@
 from app import login_required
-from flask import Blueprint
+from flask import Blueprint, render_template, jsonify
+from flask_login import current_user
 
 professor_cod = Blueprint('professor_cod', __name__, url_prefix='/prof_cod')
 
@@ -7,4 +8,6 @@ professor_cod = Blueprint('professor_cod', __name__, url_prefix='/prof_cod')
 @professor_cod.route('/home/')
 @login_required(3)
 def home():
-    return 'Professor COD Home'
+    data = {"username": current_user.username, "role": current_user.role, "email": current_user.email}
+    #return render_template("professor_cod/professor_cod.html", data=data)
+    return jsonify(data)
