6c72ed6e1cd7cf86e3a10a1be428ca464d1777a2
Daniela Craciun <craciundaniela19@gmail.com>
2016-04-12 00:48:30 +0300

    Added a bit of bootstrap

--- app/modules/student/controllers.py
+++ app/modules/student/controllers.py
@@ -1,5 +1,5 @@
 from app import login_required
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 from flask.ext.login import current_user
 
 student = Blueprint('student', __name__, url_prefix='/student')
@@ -8,7 +8,8 @@ student = Blueprint('student', __name__, url_prefix='/student')
 @student.route('/home/')
 @login_required(1)
 def home():
-    return jsonify({"username": current_user.username,
-                    "e-mail": current_user.email,
-                    "role": current_user.role,
-                    })
+    data = {"username": current_user.username,
+            "email": current_user.email,
+            "role": current_user.role,
+            }
+    return render_template('student/welcome.html', data=data)
