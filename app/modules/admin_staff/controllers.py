6eaf97c4e4050aac4da54d8bec261a4fa4e7b70a
Criss <suteucristian27@gmail.com>
2016-04-12 10:24:25 +0300

    Prints the admin user info using template with bootstrap

--- app/modules/admin_staff/controllers.py
+++ app/modules/admin_staff/controllers.py
@@ -1,10 +1,15 @@
 from app import login_required
-from flask import Blueprint
+from flask import Blueprint,render_template
+from flask.ext.login import current_user
 
 admin_staff = Blueprint('admin_staff', __name__, url_prefix='/admin')
 
 
 @admin_staff.route('/home/')
 @login_required(4)
-def home():
-    return 'Admin Home'
+def admin_home():
+    user_info = {"username": current_user.username,
+                 "email": current_user.email,
+                 "role": current_user.role,
+                 "status": current_user.status}
+    return render_template('admin/confirmedLogIn.html', user_info=user_info)
