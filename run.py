4d882bb680f4a048cb0ebca59c85ae56ed71c2f7
Robert Krisztian Sandor <sandorrobertk94@gmail.com>
2016-04-23 10:58:15 +0000

    added remember me and styled login

--- run.py
+++ run.py
@@ -4,5 +4,6 @@
 # Cloud9 app run
 import os
 from app import app
+print('http://student-management-robert-sandor-2.c9users.io/')
 app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), 
         debug=True)
