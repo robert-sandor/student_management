e579344e5ad85c70aab9af27d3ed5e32f142d1d4
dutzi <alexandratrifan95@gmail.com>
2016-04-25 23:57:34 +0300

    Attempt to create the grades page for student
    Signed-off-by: dutzi <alexandratrifan95@gmail.com>

--- app/modules/student/controllers.py
+++ app/modules/student/controllers.py
@@ -1,6 +1,8 @@
-from app import login_required
-from flask import Blueprint, jsonify, render_template
+from app import login_required, db
+from flask import Blueprint, render_template
 from flask.ext.login import current_user
+
+from app.modules.common.models import Student
 
 student = Blueprint('student', __name__, url_prefix='/student')
 
@@ -13,3 +15,14 @@ def home():
             "role": current_user.role,
             }
     return render_template('student/welcome.html', data=data)
+
+
+@student.route('/home/grades/', methods=['GET', 'POST'])
+@login_required(1)
+def grades():
+    stud = Student.query.filter_by(user_id=current_user.id).first()
+    data = {
+        "firstname": stud.first_name,
+        "lastname": stud.last_name
+    }
+    return render_template('student/temporary_grades.html', data=data)
