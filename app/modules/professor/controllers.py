9b9c0bb9c1d8c7c855f9d5bac4e3161a3b869418
Victor Ursan <victor.ursan@gmail.com>
2016-04-26 14:24:57 +0300

    added prof/grading/<course> route

--- app/modules/professor/controllers.py
+++ app/modules/professor/controllers.py
@@ -6,18 +6,28 @@ from flask_login import current_user
 professor = Blueprint('professor', __name__, url_prefix='/prof')
 
 
-@professor.route('/home/')
+@professor.route('/home/', methods=['GET'])
 @login_required(2)
 def home():
     current_proffesor = Professor.query.filter_by(id_user=current_user.get_id()).first()
-    courses = []
-    for professor_roles in current_proffesor.professor_roles:
-        if professor_roles.role_type.id == 1:
-            courses.append(professor_roles.course.course_name)
+    courses = __get_courses_for_professor(current_proffesor)
     data = {"username": current_user.username, "role": current_user.role, "email": current_user.email,
             "courses": courses}
 
     return render_template("professor/professor.html", data=data)
 
 
+@professor.route('/grading/<course>', methods=['GET'])
+@login_required(2)
+def get_students_for_course(course):
+    current_proffesor = Professor.query.filter_by(id_user=current_user.get_id()).first()
+    courses = __get_courses_for_professor(current_proffesor)
 
+    data = {"username": current_user.username, "role": current_user.role, "email": current_user.email,
+            "courses": courses, "content": course}
+
+    return render_template("professor/professor.html", data=data)
+
+
+def __get_courses_for_professor(professor):
+    return [professor_role.course.course_name for professor_role in professor.professor_roles if professor_role.role_type.id == 1]
