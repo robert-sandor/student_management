5cf323f8a2d16d506bd85537a0d3d6b24e012d2e
Victor Ursan <victor.ursan@gmail.com>
2016-04-26 15:03:17 +0300

    added welcome.html and changed professor.html in grading.html

--- app/modules/professor/controllers.py
+++ app/modules/professor/controllers.py
@@ -3,31 +3,36 @@ from app.modules.common.models import Professor, ProfessorRole, Course
 from flask import Blueprint, render_template
 from flask_login import current_user
 
-professor = Blueprint('professor', __name__, url_prefix='/prof')
+professor_blueprint = Blueprint('professor', __name__, url_prefix='/prof')
 
 
-@professor.route('/home/', methods=['GET'])
+@professor_blueprint.route('/home/', methods=['GET'])
 @login_required(2)
 def home():
-    current_proffesor = Professor.query.filter_by(id_user=current_user.get_id()).first()
-    courses = __get_courses_for_professor(current_proffesor)
+    professor = Professor.query.filter_by(id_user=current_user.get_id()).first()
+    courses = __get_professor_courses(professor)
+    courses_names = __get_courses_names(courses)
     data = {"username": current_user.username, "role": current_user.role, "email": current_user.email,
-            "courses": courses}
+            "courses_names": courses_names}
 
-    return render_template("professor/professor.html", data=data)
+    return render_template("professor/welcome.html", data=data)
 
 
-@professor.route('/grading/<course>', methods=['GET'])
+@professor_blueprint.route('/grading/<course>', methods=['GET'])
 @login_required(2)
 def get_students_for_course(course):
     current_proffesor = Professor.query.filter_by(id_user=current_user.get_id()).first()
-    courses = __get_courses_for_professor(current_proffesor)
-
+    courses = __get_professor_courses(current_proffesor)
+    courses_names = __get_courses_names(courses)
     data = {"username": current_user.username, "role": current_user.role, "email": current_user.email,
-            "courses": courses, "content": course}
+            "courses_names": courses_names, "content": course}
 
-    return render_template("professor/professor.html", data=data)
+    return render_template("professor/grading.html", data=data)
 
 
-def __get_courses_for_professor(professor):
-    return [professor_role.course.course_name for professor_role in professor.professor_roles if professor_role.role_type.id == 1]
+def __get_professor_courses(professor) -> [Course]:
+    return [professor_role.course for professor_role in professor.professor_roles if professor_role.role_type.id == 1]
+
+
+def __get_courses_names(courses) -> [str]:
+    return [course.course_name for course in courses]
