37994227657ff6380ebfbf4263cc3437061bc8a0
Daniela Craciun <craciundaniela19@gmail.com>
2016-05-16 01:20:57 +0300

    Dict of package: courses sent back to server, nothing to do with info ???

--- app/modules/professor_cod/controllers.py
+++ app/modules/professor_cod/controllers.py
@@ -1,6 +1,6 @@
 from app import login_required
 from app.modules.common.models import OptionalCourse, Course, Package
-from flask import Blueprint, render_template, jsonify, request
+from flask import Blueprint, render_template, jsonify, request, redirect, url_for
 from flask_login import current_user
 
 professor_cod = Blueprint('professor_cod', __name__, url_prefix='/prof_cod')
@@ -26,8 +26,15 @@ def proposals():
     return render_template("professor_cod/proposals.html", data=data)
 
 
-@professor_cod.route('/proposals/preview/')
+@professor_cod.route('/proposals/save/', methods=['POST'])
 @login_required(3)
-def preview_proposals():
-    data = {"username": current_user.username, "role": current_user.role, "email": current_user.email}
-    return render_template("professor_cod/proposals.html", data=data)
+def save():
+    data = {}
+    for pack in request.json.keys():
+        pack_name = Package.query.filter_by(id=int(pack)).first().name
+        courses = request.json[pack]
+        data[pack_name] = []
+        for course in courses:
+            data[pack_name].append(Course.query.filter_by(id=int(course)).first())
+        print(data)
+    return '', 200
