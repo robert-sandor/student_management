e39892cba416c422c3fafabfb000173d3a1df183
Criss <suteucristian27@gmail.com>
2016-05-29 18:50:41 +0300

    Everything proposal relating up and working

--- app/modules/professor/controllers.py
+++ app/modules/professor/controllers.py
@@ -1,5 +1,5 @@
-from app import login_required
-from flask import Blueprint, render_template, request
+from app import login_required, db
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user
 from app.modules.common.models import Professor, ProposedCourses
 from app.modules.professor.forms import ProposalForm
@@ -57,11 +57,8 @@ def add_proposal():
         form.study_line.data = ""
         form.description.data = ""
 
-        some_engine = create_engine(SQLALCHEMY_DATABASE_URI)
-        Session = sessionmaker(bind=some_engine)
-        session = Session()
-        session.add(proposal)
-        session.commit()
+        db.session.add(proposal)
+        db.session.commit()
 
     return render_template("professor/add_proposal.html", data=data, form=form)
 
@@ -79,3 +76,61 @@ def view_proposals():
             "max_proposals": 2,
             "proposals": proposed_courses}
     return render_template("professor/view_proposals.html", data=data)
+
+
+@professor.route('/proposals/update_proposal/', methods=['GET', 'POST'])
+@professor.route('/proposals/update_proposal/<proposal_id>/', methods=['GET', 'POST'])
+@login_required(2)
+def update_proposal(proposal_id=None):
+    if proposal_id is None:
+        return redirect(url_for('404'))
+    prof = Professor.query.filter_by(id_user=current_user.id).first()
+    proposed_courses = ProposedCourses.query.filter_by(professor_id=prof.id).all()
+    proposed_course = ProposedCourses.query.filter_by(id=proposal_id).first()
+    form = ProposalForm(request.form)
+    ranks = {"doctorand": 0,
+             "asistent": 1,
+             "conferentiar": 2,
+             "lector": 3,
+             "prof": 4}
+
+    data = {"username": current_user.username,
+            "role": current_user.role,
+            "email": current_user.email,
+            "rank_prof": prof.rank,
+            "ranks": ranks,
+            "no_proposals": proposed_courses.__len__(),
+            "max_proposals": 2,
+            "proposal": proposed_course}
+
+    if form.validate_on_submit():
+        proposal = ProposedCourses(prof.id, form.course_name.data, form.speciality.data, form.study_line.data,
+                                   form.description.data)
+        form.course_name.data = ""
+        form.speciality.data = ""
+        form.study_line.data = ""
+        form.description.data = ""
+
+        pc = db.session.query(ProposedCourses).get(proposal_id)
+
+        pc.course_name = proposal.course_name
+        pc.speciality = proposal.speciality
+        pc.study_line = proposal.study_line
+        pc.description = proposal.description
+
+        db.session.commit()
+        return redirect(url_for("professor.view_proposals"))
+    return render_template("professor/update_proposal.html", data=data, form=form)
+
+
+@professor.route('/proposals/delete_proposal/')
+@professor.route('/proposals/delete_proposal/<proposal_id>')
+@login_required(2)
+def delete_proposal(proposal_id=None):
+    if proposal_id is None:
+        return redirect(url_for('404'))
+
+    proposed_course = ProposedCourses.query.filter_by(id=proposal_id).first()
+    db.session.delete(proposed_course)
+    db.session.commit()
+    return redirect(url_for("professor.view_proposals"))
